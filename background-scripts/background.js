688a0ff445bc95af2373e5fe8b3867c7415fe3ca
Harrenvolk <aniruddhpatil07@live.com>
2022-02-07 23:10:40 +0530

    feat : URL Blocking before Request

--- background-scripts/background.js
+++ background-scripts/background.js
@@ -1,5 +1,13 @@
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
-      chrome.action.setBadgeText(details={tabId,text:"Hi"})
-    }
-  })
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+    console.log("Loading page")
+  }
+})
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    console.log("Blocked page")
+    return {cancel: details.url.indexOf("https://www.xn--80ak6aa92e.com/") != -1};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking"]
+);
