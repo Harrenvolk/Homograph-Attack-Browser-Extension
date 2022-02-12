fb51e8dcea3fbdd197e13b83e43dfbdbb60dd50e
Sriram2001 <sriramudupi19@gmail.com>
2022-02-12 23:48:29 +0530

    feat: API request from popup

--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
 console.log("Listening for message")
 window.onload = function() {
     document.getElementById("b1").onclick = function () {
-        chrome.tabs.create({url:"https://www.google.com"})
+        // chrome.tabs.create({url:"https://www.google.com"})
+        fetch('https://random.justyy.workers.dev/api/random/?cached&n=128')
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(err => console.error(err))
     }
 }
 console.log("Ending", chrome)
