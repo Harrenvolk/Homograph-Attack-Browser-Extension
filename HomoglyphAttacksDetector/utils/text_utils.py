fccd4bbecc7b5d92ae77ef10ac56e14e4894c982
shivamanipatil <shivamanipatil.181it144@nitk.edu.in>
2022-02-15 01:22:47 +0530

    KD trees

---
+++ HomoglyphAttacksDetector/utils/text_utils.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+from keras import backend as K
+
+
+def euclidean_distance(vects):
+    x, y = vects
+    return K.sqrt(K.sum(K.square(x - y)+np.random.rand()*.0001, axis=1, keepdims=True))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
