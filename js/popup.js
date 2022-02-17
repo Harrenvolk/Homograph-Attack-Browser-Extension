d5004ba97d073adf2c8bab6866c4f75771fb68cc
Sriram2001 <sriramudupi19@gmail.com>
2022-02-18 02:37:49 +0530

    UI bugfixes

---
+++ js/popup.js
@@ -0,0 +1,44 @@
+console.log("Listening for message")
+window.onload = function () {
+    chrome.storage.sync.get(['url'], function (result) {
+        document.getElementById("b1").onclick = function () {
+            chrome.storage.sync.set({ redirected: result.url });
+            window.close();
+        }
+        $('#dummy').on('click', () => {
+            
+            const URL = "https://5041-202-164-132-133.ngrok.io/predict?url=" + result.url.split('://')[1].replace('/', '').replace('www.', '');
+            // const URL = "https://5041-202-164-132-133.ngrok.io/predict?url=" + result.url.replace(/^http+:\/\//, '').replace('/', '').replace('www.', '');
+            // $("#safe").text(result.url + '   ' + URL);
+            fetch(URL)
+                .then(r => r.json())
+                .then(r => {
+                    if (r.verdict === 'safe') {
+                        $('#safe').text('safe!!');
+                        setTimeout(()=>$('#b1').click(),1);
+                    } else {
+                        $('#suggested_links').css('display', 'block');
+                        $('#safe').css('display', 'none');
+                        const list = $('.links');
+                        r.suggestions.forEach((suggestion) => {
+                            list.append($('<li/>')
+                                .text(suggestion)
+                                .on('click', () => {
+                                    chrome.tabs.create({ url: "https://" + suggestion });
+                                    window.close();
+                                }))
+                        })
+                    }
+                })
+                .catch(err => console.error(err));
+
+        })
+    })
+
+    // document.getElementById("closebutton").onclick = function () {
+    //     window.close();
+    // }
+    setTimeout(() => $("#dummy").click(), 1);
+    $('#closebutton').on('click', () => { window.close() })
+
+}
